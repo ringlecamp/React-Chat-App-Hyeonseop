600cd5cc0d8c33e5a25c7c3d965e4c2789cfcbf6
HyeonseopJeong <gustjq1101@naver.com>
2021-06-30 04:34:04 +0900

    make boilerplate for each pages

---
+++ src/pages/ChatList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChatList() {
+    return <h1>Chat List</h1>
+}
+
+export default ChatList;
---
+++ src/pages/ChatRoom.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChatRoom() {
+    return <h1>Chat Room</h1>
+}
+
+export default ChatRoom;
